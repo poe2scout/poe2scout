f584e2713255923c245343dbdb8fd8ec81317337
Blake Girardet <blake.girardet@halosystems.co.nz>
2025-04-13 10:56:36 +1200

    made cache busting request non blocking

--- frontend/vite-project/src/App.tsx
+++ frontend/vite-project/src/App.tsx
@@ -5,10 +5,11 @@ import LandingPage from "./pages/LandingPage";
 import EconomyPage from "./pages/EconomyPage";
 import { LanguageProvider } from "./contexts/LanguageContext";
 import { version } from "../package.json";
-import CacheBuster from "react-cache-buster";
+import { useEffect } from "react";
 import { LeagueProvider } from "./contexts/LeagueContext";
 import fetchIntercept from 'fetch-intercept';
 import { CategoryProvider } from './contexts/CategoryContext';
+import { compare } from 'compare-versions';
 
 // Register fetch interceptor
 fetchIntercept.register({
@@ -21,44 +22,69 @@ fetchIntercept.register({
 });
 
 function App() {
-  const theme = createTheme({
-    palette: {
-      mode: "dark",
-    },
-  });
+    const theme = createTheme({
+        palette: {
+            mode: "dark",
+        },
+    });
 
-  return (
-    <CacheBuster
-      currentVersion={version}
-      isEnabled={true}
-      isVerboseMode={false}
-    >
-      <ThemeProvider theme={theme}>
-        <LanguageProvider>
-          <LeagueProvider>
-            <CategoryProvider>
-              <CssBaseline />
-              <BrowserRouter>
-                <Routes>
-                  <Route path="/" element={<Layout />}>
-                    <Route index element={<LandingPage />} />
-                    <Route path="economy">
-                      <Route
-                        index
-                        element={<Navigate to="/economy/currency" replace />}
-                      />
-                      <Route path=":type" element={<EconomyPage />} />
-                    </Route>
-                    <Route path="*" element={<Navigate to="/" replace />} />
-                  </Route>
-                </Routes>
-              </BrowserRouter>
-            </CategoryProvider>
-          </LeagueProvider>
-        </LanguageProvider>
-      </ThemeProvider>
-    </CacheBuster>
-  );
+    useEffect(() => {
+        const checkCacheStatus = async () => {
+            try {
+                const res = await fetch('/meta.json');
+                const { version: metaVersion } = await res.json();
+                
+                const shouldForceRefresh = compare(metaVersion, version, '>');
+                if (shouldForceRefresh) {
+                    console.log(`There is a new version (v${metaVersion}). Should force refresh.`);
+                    
+                    if (window?.caches) {
+                        const { caches } = window;
+                        const cacheNames = await caches.keys();
+                        const cacheDeletionPromises = cacheNames.map((n) => caches.delete(n));
+                        await Promise.all(cacheDeletionPromises);
+                        console.log('The cache has been deleted.');
+                    }
+                    
+                    // @ts-ignore: Firefox still has a `forceReload` parameter.
+                    window.location.reload(true);
+                } else {
+                    console.log('There is no new version. No cache refresh needed.');
+                }
+            } catch (error) {
+                console.error('An error occurred while checking cache status:', error);
+            }
+        };
+
+        checkCacheStatus();
+    }, []);
+
+    return (
+        <ThemeProvider theme={theme}>
+            <LanguageProvider>
+                <LeagueProvider>
+                    <CategoryProvider>
+                        <CssBaseline />
+                        <BrowserRouter>
+                            <Routes>
+                                <Route path="/" element={<Layout />}>
+                                    <Route index element={<LandingPage />} />
+                                    <Route path="economy">
+                                        <Route
+                                            index
+                                            element={<Navigate to="/economy/currency" replace />}
+                                        />
+                                        <Route path=":type" element={<EconomyPage />} />
+                                    </Route>
+                                    <Route path="*" element={<Navigate to="/" replace />} />
+                                </Route>
+                            </Routes>
+                        </BrowserRouter>
+                    </CategoryProvider>
+                </LeagueProvider>
+            </LanguageProvider>
+        </ThemeProvider>
+    );
 }
 
 export default App;
