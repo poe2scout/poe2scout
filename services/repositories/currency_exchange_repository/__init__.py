80df53507e532fb5df4b02041b8c927b66b5f92c
Blake Girardet <blake.girardet@halosystems.co.nz>
2025-09-03 23:28:32 +1200

    new currencyexchange snapshot stuff

--- services/repositories/currency_exchange_repository/__init__.py
+++ services/repositories/currency_exchange_repository/__init__.py
@@ -1,7 +1,11 @@
+from services.repositories.currency_exchange_repository.GetCurrencyExchange import GetCurrencyExchange
 from services.repositories.currency_exchange_repository.CreateSnapshot import CreateSnapshot
+from services.repositories.currency_exchange_repository.GetCurrencyExchangeHistory import GetCurrencyExchangeHistory
 from services.repositories.currency_exchange_repository.GetLastFetchedEpoch import GetLastFetchedEpoch
 
 class CurrencyExchangeRepository:
     def __init__(self):
         self.GetLastFetchedEpoch = GetLastFetchedEpoch().execute
         self.CreateSnapshot = CreateSnapshot().execute
+        self.GetCurrencyExchange = GetCurrencyExchange().execute
+        self.GetCurrencyExchangeHistory = GetCurrencyExchangeHistory().execute
