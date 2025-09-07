663e816b058c7e54869c328a32991de9c28bd285
Blake Girardet <48109706+Caidora@users.noreply.github.com>
2025-09-08 00:02:07 +1200

    more efficient price history query

--- services/repositories/item_repository/GetItemPriceHistory.py
+++ services/repositories/item_repository/GetItemPriceHistory.py
@@ -16,29 +16,16 @@ class GetItemPriceHistory(BaseRepository):
         limit = logCount + 1
 
         price_log_query = """
-            WITH binned_logs AS (
-                SELECT 
-                    price,
-                    quantity,
-                    "createdAt",
-                    ROW_NUMBER() OVER (
-                        PARTITION BY date_bin((%(logFrequency)s || ' hours')::interval, "createdAt", %(endTime)s::timestamp) 
-                        ORDER BY "createdAt" DESC
-                    ) as rn
-                FROM "PriceLog"
-                WHERE 
-                    "itemId" = %(itemId)s
-                    AND "leagueId" = %(leagueId)s
-                    AND "createdAt" < %(endTime)s
-            )
-            SELECT 
-                price,
-                quantity,
-                date_bin((%(logFrequency)s || ' hours')::interval, "createdAt", %(endTime)s::timestamp) as time
-            FROM binned_logs
-            WHERE rn = 1 
-            ORDER BY time DESC 
-            LIMIT %(limit)s;
+SELECT DISTINCT ON (time)
+       price,
+       quantity,
+       date_bin((%(logFrequency)s || ' hours')::interval, "createdAt", %(endTime)s::timestamp) as time
+  FROM "PriceLog"
+ WHERE "itemId" = %(itemId)s
+       AND "leagueId" = %(leagueId)s
+       AND "createdAt" < %(endTime)s
+ ORDER BY time DESC,"createdAt" DESC
+ LIMIT %(limit)s;
         """
 
         query_params = {
