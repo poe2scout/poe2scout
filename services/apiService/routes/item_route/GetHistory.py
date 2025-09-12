2c9b8e8969f844266d24c9b5da6af8bb090a7622
Blake Girardet <blake.girardet@halosystems.co.nz>
2025-09-13 08:27:55 +1200

    Revert "fixes item price history not including latest price"
    This reverts commit a4b3f910e68a03fc23b19c4289bbb973806750b3.

--- services/apiService/routes/item_route/GetHistory.py
+++ services/apiService/routes/item_route/GetHistory.py
@@ -27,9 +27,16 @@ async def GetHistory(itemId: int, league: str, logCount: int, endTime: datetime 
     isACurrency = await item_repository.IsItemACurrency(itemId)
 
     if isACurrency:
+
         logFrequency = 1
     else:
         logFrequency = 6
+    
+    endTime = endTime.replace(
+        hour=(endTime.hour// logFrequency) * logFrequency,
+        minute=0,
+        second=0,
+        microsecond=0)
     
     history = await item_repository.GetItemPriceHistory(itemId, leagueId, logCount, logFrequency, endTime)
 
