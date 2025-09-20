3935e4f02a1d8b0ac87a6dddec81d26ab023f5c2
Blake Girardet <blake.girardet@halosystems.co.nz>
2025-09-20 17:41:03 +1200

    Caches current snapshot

--- services/apiService/routes/currency_exchange_route/GetSnapshotPairs.py
+++ services/apiService/routes/currency_exchange_route/GetSnapshotPairs.py
@@ -4,9 +4,9 @@ from services.repositories.currency_exchange_repository.GetCurrentSnapshotPairs 
 )
 from . import router
 from fastapi import HTTPException
-from services.apiService.dependancies import CXRepoDep, ItemRepoDep
+from services.apiService.dependancies import CXRepoDep, ItemRepoDep, cache_response
 
-
+@cache_response(lambda: "SnapshotPairs", 60 * 10 )
 @router.get("/SnapshotPairs")
 async def GetSnapshotPairs(
     league: str, item_repo: ItemRepoDep, cx_repo: CXRepoDep
