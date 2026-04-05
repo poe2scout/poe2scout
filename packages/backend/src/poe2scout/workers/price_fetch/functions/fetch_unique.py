650c912fad21b9a59fe4ab91b81227285cdbcfea
Blake Girardet <blake.girardet@halosystems.co.nz>
2026-04-05 20:58:02 +1200

    stop crash on empty list

--- packages/backend/src/poe2scout/workers/price_fetch/functions/fetch_unique.py
+++ packages/backend/src/poe2scout/workers/price_fetch/functions/fetch_unique.py
@@ -85,17 +85,13 @@ async def fetch_unique(
 
     fetch_data = fetch_response.json()
 
-    if "item" not in fetch_data["result"][0]:
-        logger.exception(f"malformed result item: {fetch_data["result"][0]}")
-        raise Exception("Malformed result item from poe2 api.")
-
-    await sync_metadata_and_icon(fetch_data["result"][0]["item"], unique_item, repo)
-
     prices = parse_trade_response(fetch_data)
 
     if len(prices) == 0:
         logger.info(f"No prices found for {unique_item.name} in {league}")
         return PriceFetchResult(price=-1, quantity=0, currency=currency)
+        
+    await sync_metadata_and_icon(fetch_data["result"][0]["item"], unique_item, repo)
 
     return PriceFetchResult(
         price=prices[0], quantity=query_data["total"], currency=currency
