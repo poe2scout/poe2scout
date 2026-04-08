37197d552824e559e072717ef5613715eb101043
Blake Girardet <blake.girardet@halosystems.co.nz>
2026-04-08 18:35:58 +1200

    Fixes.

--- packages/backend/src/poe2scout/workers/item_sync/__init__.py
+++ packages/backend/src/poe2scout/workers/item_sync/__init__.py
@@ -1,6 +1,11 @@
-from .service import run
+from .service import run_once
 from .functions.sync_items import sync_items
 from .functions.sync_currencies import sync_currencies
 from .config import ItemSyncConfig
 
-__all__ = ["run", "sync_items", "sync_currencies", "ItemSyncConfig"]
+__all__ = [
+    "run_once",
+    "sync_items",
+    "sync_currencies",
+    "ItemSyncConfig",
+]
