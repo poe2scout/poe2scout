0631ffb4d74fcbd6215d0393d21f4bd7ac1d1048
Blake Girardet <blake.girardet@halosystems.co.nz>
2026-04-19 17:48:35 +1200

    fixes slow query

--- packages/backend/src/poe2scout/db/repositories/league_repository/get_items_in_current_league.py
+++ packages/backend/src/poe2scout/db/repositories/league_repository/get_items_in_current_league.py
@@ -4,11 +4,30 @@ from ..base_repository import BaseRepository, scalar_as
 async def get_items_in_current_league(league_id: int, realm_id: int) -> list[int]:
     async with BaseRepository.get_db_cursor(row_factory=scalar_as(int)) as cursor:
         query = """
-            SELECT DISTINCT pl.item_id
-              FROM price_log AS pl
-             WHERE pl.league_id = %s
-               AND pl.realm_id = %s;
+WITH RECURSIVE distinct_items AS (
+    (
+        SELECT item_id 
+        FROM price_log 
+        WHERE league_id = %(league_id)s AND realm_id = %(realm_id)s 
+        ORDER BY item_id 
+        LIMIT 1
+    )
+    UNION ALL
+    SELECT (
+        SELECT item_id 
+        FROM price_log 
+        WHERE league_id = %(league_id)s AND realm_id = %(realm_id)s
+          AND item_id > distinct_items.item_id 
+        ORDER BY item_id 
+        LIMIT 1
+    )
+    FROM distinct_items
+    WHERE distinct_items.item_id IS NOT NULL
+)
+SELECT item_id 
+FROM distinct_items
+WHERE item_id IS NOT NULL
         """
-        await cursor.execute(query, (league_id, realm_id))
+        await cursor.execute(query, {"league_id": league_id, "realm_id": realm_id})
 
         return await cursor.fetchall()
