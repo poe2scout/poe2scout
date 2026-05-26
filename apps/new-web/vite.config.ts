374cf682ab62fc03de3226710126ad900c5d77ed
Blake Girardet <blake.girardet@halosystems.co.nz>
2026-05-26 21:44:44 +1200

    Update packages.

--- apps/new-web/vite.config.ts
+++ apps/new-web/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: "https://api.poe2scout.com",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
